e43aeb843adac43787a2e60d175474a4e2b64b2f
Leonardo Zanette <leonardobzanette@gmail.com>
2022-05-20 16:37:54 -0300

    Requisito 1 - DONE

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqubi
+function getSpeciesByIds(...ids) {
+  // if (ids === undefined) {
+  //   return []
+  // }
+  // return [
+  //   ids.forEach(element => species.find((a) => a.id === element))
+  // ]
+  const resultado = [];
+
+  ids.forEach((element) => {
+    const especie = species.find((a) => a.id === element);
+    resultado.push(especie);
+  });
+
+  return resultado;
 }
 
 module.exports = getSpeciesByIds;
