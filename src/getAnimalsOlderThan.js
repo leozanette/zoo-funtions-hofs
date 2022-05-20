92206825357ecfe316e26dd99d5615ae5acd4172
Leonardo Zanette <leonardobzanette@gmail.com>
2022-05-20 16:54:47 -0300

    Requisito 2 - DONE

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.filter((a) => a.name === animal)[0].residents.every((b) => b.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
