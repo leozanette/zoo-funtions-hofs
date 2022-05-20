086fd2569bda3b0eebf04271ac9758ef6d2baf6b
Leonardo Zanette <leonardobzanette@gmail.com>
2022-05-20 18:13:58 -0300

    Requisito 3 - DONE

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const result = {};
+
+  const funcionario = employees.find((a) => a.firstName === employeeName
+    || a.lastName === employeeName);
+
+  if (employeeName === undefined) {
+    return result;
+  }
+
+  return funcionario;
 }
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,12 +2,6 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  // if (ids === undefined) {
-  //   return []
-  // }
-  // return [
-  //   ids.forEach(element => species.find((a) => a.id === element))
-  // ]
   const resultado = [];
 
   ids.forEach((element) => {
