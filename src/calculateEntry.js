c39381818afb906d2f95fe98627dfa6f88c90c74
Leonardo Zanette <leonardobzanette@gmail.com>
2022-05-23 18:10:28 -0300

    Requisito 8 - DONE

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const adult = entrants.filter((a) => a.age >= 18 && a.age < 50).length;
+  const child = entrants.filter((a) => a.age < 18).length;
+  const senior = entrants.filter((a) => a.age >= 50).length;
+  const obj = {
+    adult,
+    child,
+    senior,
+  };
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const resumo = countEntrants(entrants);
+  const total = (resumo.adult * prices.adult) + (resumo.child * prices.child)
+  + (resumo.senior * prices.senior);
+
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
