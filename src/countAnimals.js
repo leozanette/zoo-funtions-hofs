beb967ad3e2ef4f76f47edf47e7aaa1cb2e30c4f
Leonardo Zanette <leonardobzanette@gmail.com>
2022-05-23 15:01:55 -0300

    Requisito 5 - DONE

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const newObj = {};
+  const residentesTotal = species.map((a) => a.residents.length);
+  const especieTotal = species.map((a) => a.name);
+
+  especieTotal.forEach((a, b) => { newObj[a] = residentesTotal[b]; });
+
+  if (animal === undefined) {
+    return newObj;
+  }
+
+  const especie = animal.specie;
+  const sexo = animal.sex;
+  const residentes = species.find((a) => a.name === especie).residents;
+  const mascFem = residentes.filter((a) => a.sex === sexo);
+
+  if (sexo === undefined) {
+    return residentes.length;
+  } return mascFem.length;
 }
 
 module.exports = countAnimals;
