4f314925e735d0196848c297bf3d0cfb40174fe6
Leonardo Zanette <leonardobzanette@gmail.com>
2022-05-24 13:00:51 -0300

    Requisito 10 - DONE

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idAnimal = employees.find((a) => a.id === id).responsibleFor[0];
+  const animalOldest = species.find((a) => a.id === idAnimal).residents
+    .reduce((a, c) => (a.age > c.age ? a : c));
+  const resultado = [animalOldest.name, animalOldest.sex, animalOldest.age];
+
+  return resultado;
 }
 
 module.exports = getOldestFromFirstSpecies;
